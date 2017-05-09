7622c5f84f4ee2d85b3166ad00918dc931840850
prashantpawase <pawaseprashant007@gmail.com>
2017-05-09 12:36:23 +0530

    fixed changes

--- src/automationFramework/MEMU.java
+++ src/automationFramework/MEMU.java
@@ -27,7 +27,8 @@ public class MEMU
 //  DeviceName = "Samsung Galaxy S4";
 //  DeviceName = "Samsung Galaxy Note 3";
 
-        DeviceName = "Apple iPhone 5";
+          DeviceName = "Apple iPhone 5";
+//        DeviceName = "Apple iPhone 6";
 
 
         System.setProperty("webdriver.chrome.driver", "/home/edn/Git-Repo/External-Jars/chromedriver");
@@ -44,7 +45,8 @@ public class MEMU
         WebDriver driver = new ChromeDriver(capabilities);
 
         driver.manage().window().maximize();
-        driver.navigate().to("http://www.edunuts.com");
+        //  driver.navigate().to("http://www.edunuts.com");
+          driver.navigate().to("http://www.bigradar.io");
 
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
